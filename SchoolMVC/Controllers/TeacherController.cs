730cfbd663f1ff84a7ad01a6b490bd6f66488405
Rjay012 <rjhaybaterina12@gmail.com>
2020-03-27 09:10:13 +0800

    Teacher

---
+++ SchoolMVC/Controllers/TeacherController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolMVC.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table(string queryString)
+        {
+            return PartialView("Partials/Tables/_TeacherTable");
+        }
+    }
+}
---
+++ SchoolMVC/Scripts/Teacher.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    LoadTeacherTable("");
+});
+
+function LoadTeacherTable(searchString) {
+    FetchData("/Teacher/Table", { searchString: searchString }).done(function (table) {
+        $("#TeacherTableForm").html(table);
+    });
+}
